cba041751234037acdf45364a9a5255fda1df41d
Ion Utale <ion.utale@guccidigital.com>
2023-06-08 15:27:26 +0200

    Initial Commit

---
+++ clean-keyboard/clean_keyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  clean_keyboardApp.swift
+//  clean-keyboard
+//
+//  Created by Ion Utale on 08/06/23.
+//
+
+import SwiftUI
+
+@main
+struct clean_keyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
